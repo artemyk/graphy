bdf59b551ba883f42e59760501b40ab3b96dbcf8
Artemy Kolchinsky <akolchin@indiana.edu>
2015-01-20 17:33:07 -0500

    Doc update

--- docs/conf.py
+++ docs/conf.py
@@ -32,6 +32,10 @@ sys.path.append(os.path.abspath('sphinxext'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'matplotlib.sphinxext.mathmpl',
+    'matplotlib.sphinxext.only_directives',
+    'matplotlib.sphinxext.plot_directive',
+    'matplotlib.sphinxext.ipython_directive',
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
