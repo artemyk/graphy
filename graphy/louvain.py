2e634163e464b9ca27067c7aae8a276ebf5965fa
Giovanni Luca Ciampaglia <gciampag@indiana.edu>
2015-02-23 15:57:15 -0500

    PEP8 style fixes + fixed bug that caused writing of input file to get stuck at edges lines

--- graphy/louvain.py
+++ graphy/louvain.py
@@ -1,6 +1,6 @@
-"""Module for running Louvain community detection algorithm.
 """
-
+Module for running Louvain community detection algorithm.
+"""
 
 from __future__ import division, print_function, absolute_import
 import six
@@ -12,140 +12,155 @@ import tempfile
 import os
 import scipy.sparse as sp
 from itertools import izip
+from contextlib import closing
 
 
 def _edge_lines_iter(conn_mx, is_sparse):
     """
     Iterate over Pajek edge lines
     """
-    for ndx in range(conn_mx.shape[0]):
-        r = conn_mx[ndx,:]
-        if is_sparse:
-            conns = r.rows[0]
-            weights = r.data[0]
-        else:
+    if is_sparse:
+        for ndx in range(conn_mx.shape[0]):
+            conns = conn_mx.rows[ndx]
+            weights = conn_mx.data[ndx]
+            if not len(conns):
+                yield ('%d %d %0.5f 0\n' % (ndx, 0, 0))
+            else:
+                for c, w in izip(conns, weights):
+                    yield ('%d %d %0.5f 0\n' % (ndx, c, w))
+    else:
+        for ndx in range(conn_mx.shape[0]):
+            r = conn_mx[ndx, :]
             conns = np.flatnonzero(r)
             weights = r[conns]
-        if not len(conns):
-            yield ('%d %d %0.5f 0\n' % (ndx, 0, 0))
-        for c, w in izip(conns, weights):
-            yield ('%d %d %0.5f 0\n' % (ndx, c, w))
+            if not len(conns):
+                yield ('%d %d %0.5f 0\n' % (ndx, 0, 0))
+            else:
+                for c, w in izip(conns, weights):
+                    yield ('%d %d %0.5f 0\n' % (ndx, c, w))
 
 
 def optimize_modularity(conn_mx, rand_init=True, num_runs=1, debug=False):
-  """Optimize directed, weighted Newman's modularity
-  using the Louvain algorithm. This uses C++ implementation from
-  https://github.com/raldecoa/SurpriseMe/tree/master/src/CPM
+    """
+    Optimize directed, weighted Newman's modularity
+    using the Louvain algorithm. This uses C++ implementation from
+    https://github.com/raldecoa/SurpriseMe/tree/master/src/CPM
 
-  For example:
+    For example:
 
-  >>> import networkx as nx
-  >>> import graphy
-  >>> G = nx.karate_club_graph()
-  >>> best_membership, q = graphy.louvain.optimize_modularity(nx.to_numpy_matrix(G))
-  >>> print(best_membership, q)   # doctest: +SKIP
+    >>> import networkx as nx
+    >>> from graphy.louvain import optimize_modularity
+    >>> G = nx.karate_club_graph()
+    >>> best_membership, q = optimize_modularity(nx.to_numpy_matrix(G))
+    >>> print(best_membership, q)   # doctest: +SKIP
 
 
-  Parameters
-  ----------
-  conn_mx : 2-dimensional np.array or scipy.sparse matrix
-    Connectivity matrix.
-  rand_init : bool (default True)
-    Whether to randomly shuffle order of nodes (makes results non-deterministic)
-  num_runs : int (default 1)
-    How many runs to perform (highest quality run returned).  Only allow if
-    rand_init is True.
-  debug : bool (default False)
-    If True, prints various debugging information.
+    Parameters
+    ----------
+    conn_mx : 2-dimensional np.array or scipy.sparse matrix
+        Connectivity matrix.
 
-  Returns
-  -------
-  np.array
-    Optimal membership vector indicating community membership of each node.
-  float
-    Modularity value corresponding to the optimal membership vector. Notice that
-    because the modularity value is computed by adding up increments over
-    many moves, this may only be accurate to a few decimal places.
+    rand_init : bool (default True)
+        Whether to randomly shuffle order of nodes (makes results
+        non-deterministic)
 
-  """
-  # TODO: Implement multithreading?  Code seems to support it already
+    num_runs : int (default 1)
+        How many runs to perform (highest quality run returned).  Only allow if
+        rand_init is True.
 
-  if num_runs > 1 and not rand_init:
-    raise ValueError('Multiple runs only makes sense when initial order of'
-                     'nodes is randomized')
+    debug : bool (default False)
+        If True, prints various debugging information.
 
-  is_sparse = sp.isspmatrix(conn_mx)
-  if is_sparse:
-      # transform to list of lists
-      conn_mx = conn_mx.tolil()
-  else:
-      conn_mx = np.asarray(conn_mx)
+    Returns
+    -------
+    np.array
+        Optimal membership vector indicating community membership of each node.
 
-  if conn_mx.shape[0] != conn_mx.shape[1]:
-      raise ValueError('conn mx should be square')
+    float
+        Modularity value corresponding to the optimal membership vector. Notice
+        that because the modularity value is computed by adding up increments
+        over many moves, this may only be accurate to a few decimal places.
+    """
+    # TODO: Implement multithreading?  Code seems to support it already
 
-  DIR = tempfile.gettempdir()
-  pfx = str(os.getpid()) + '_'
-  NETWORK_FILE = os.path.join(DIR, pfx+'net.txt')
-  OUTPUT_FILE  = os.path.join(DIR, pfx+'net.bin')
-  NODEMAP_FILE = os.path.join(DIR, pfx+'nmap.bin')
-  CONF_FILE    = os.path.join(DIR, pfx+'conf.bin')
+    # check inputs
+    if num_runs > 1 and not rand_init:
+        raise ValueError('Multiple runs only makes sense when initial order of'
+                         'nodes is randomized')
 
-  with open(NETWORK_FILE, 'w') as f:
-      f.write('>\n')
-      N_nodes = conn_mx.shape[0]
-      node_lines = ('%d %d\n' % (ndx, ndx) for ndx in range(N_nodes))
-      f.writelines(node_lines)
-      f.write('>\n0 0\n>\n')
-      edges_lines = _edge_lines_iter(conn_mx, is_sparse)
-      f.writelines(edges_lines)
-  if debug:
-    print("**** NETWORK FILE: ****")
-    with open(NETWORK_FILE) as f:
-      print(f.read())
-    print()
+    # check is sparse or not
+    is_sparse = sp.isspmatrix(conn_mx)
+    if is_sparse:
+        # transform to list of lists
+        conn_mx = conn_mx.tolil()
+    else:
+        conn_mx = np.asarray(conn_mx)
 
-  bin_dir = os.path.join(os.path.dirname(__file__),'..','external','SurpriseMeCPM','bin')
-  subprocess.call([os.path.join(bin_dir,'slicer'),
-                   '-i', NETWORK_FILE,
-                   '-o', OUTPUT_FILE,
-                   '-n', NODEMAP_FILE,
-                   '-c', CONF_FILE], stderr=subprocess.PIPE)
+    # check is square matrix
+    if conn_mx.shape[0] != conn_mx.shape[1]:
+        raise ValueError('conn mx should be square')
 
-  call_opts = [os.path.join(bin_dir,'community'),]
-  if rand_init:
-    call_opts.append('-r')
-  call_opts.append(OUTPUT_FILE)
-  call_opts.append(CONF_FILE)
+    # create files
+    DIR = tempfile.gettempdir()
+    pfx = str(os.getpid()) + '_'
+    NETWORK_FILE = os.path.join(DIR, pfx+'net.txt')
+    OUTPUT_FILE = os.path.join(DIR, pfx+'net.bin')
+    NODEMAP_FILE = os.path.join(DIR, pfx+'nmap.bin')
+    CONF_FILE = os.path.join(DIR, pfx+'conf.bin')
 
-  best_membership, best_q = None, None
-  for run_ndx in range(num_runs):
-    res = subprocess.Popen(call_opts, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    with closing(open(NETWORK_FILE, 'w')) as f:
+        f.write('>\n')
+        N_nodes = conn_mx.shape[0]
+        node_lines = ('%d %d\n' % (ndx, ndx) for ndx in range(N_nodes))
+        f.writelines(node_lines)
+        f.write('>\n0 0\n>\n')
+        edges_lines = _edge_lines_iter(conn_mx, is_sparse)
+        f.writelines(edges_lines)
 
-    output, errors = map(lambda s: s.decode('ascii'), res.communicate())
+    bin_dir = os.path.join(os.path.dirname(__file__), '..', 'external',
+                           'SurpriseMeCPM', 'bin')
+    subprocess.call([os.path.join(bin_dir, 'slicer'),
+                     '-i', NETWORK_FILE,
+                     '-o', OUTPUT_FILE,
+                     '-n', NODEMAP_FILE,
+                     '-c', CONF_FILE], stderr=subprocess.PIPE)
 
-    all_files = [NETWORK_FILE, OUTPUT_FILE, NODEMAP_FILE, CONF_FILE]
-    if debug:
-      print("******* RUN %d *******" % run_ndx)
-      print("**** OUTPUT: ****")
-      print(output)
-      print()
-      print("**** STDERR: ****")
-      print(errors)
-      print("*****************")
-      print("Leaving files in place:")
-      print(all_files)
+    call_opts = [os.path.join(bin_dir, 'community'), ]
+    if rand_init:
+        call_opts.append('-r')
+    call_opts.append(OUTPUT_FILE)
+    call_opts.append(CONF_FILE)
+
+    best_membership, best_q = None, None
+    for run_ndx in range(num_runs):
+        res = subprocess.Popen(call_opts, stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+
+        output, errors = map(lambda s: s.decode('ascii'), res.communicate())
+
+        all_files = [NETWORK_FILE, OUTPUT_FILE, NODEMAP_FILE, CONF_FILE]
+        if debug:
+            print("******* RUN %d *******" % run_ndx)
+            print("**** OUTPUT: ****")
+            print(output)
+            print()
+            print("**** STDERR: ****")
+            print(errors)
+            print("*****************")
+            print("Leaving files in place:")
+            print(all_files)
 
     q_value = float(errors.strip().split("\n")[-1].split(" ")[0])
     membership = np.zeros(conn_mx.shape[0], dtype='int')
-    ndxs, vals = zip(*[map(int, l.strip().split("\t")) for l in output.strip().split("\n")])
+    ndxs, vals = zip(*[map(int, l.strip().split("\t"))
+                       for l in output.strip().split("\n")])
     membership[list(ndxs)] = vals
 
     if best_q is None or q_value > best_q:
-      best_membership, best_q = membership, q_value
+        best_membership, best_q = membership, q_value
 
-  if not debug:
-    for fname in all_files:
-      os.remove(fname)
+    if not debug:
+        for fname in all_files:
+            os.remove(fname)
 
-  return best_membership, best_q
+    return best_membership, best_q
