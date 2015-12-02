ac7f3c15bd08718dd0e04ec3ce9c763f3d8125b8
Artemy Kolchinsky <akolchin@indiana.edu>
2015-12-02 12:34:43 -0500

    Copyright update

--- docs/conf.py
+++ docs/conf.py
@@ -79,7 +79,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'graphy'
-copyright = u'2013-2014, Artemy Kolchinsky'
+copyright = u'2013-2015, Artemy Kolchinsky'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
